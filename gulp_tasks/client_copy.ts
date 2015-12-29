1b4b5a3c1e11bc9a6197616ccbd77019f564dfbc
Henry Zektser <hzektser@Henrys-MBP.home>
2015-12-29 16:39:32 -0500

    Frontend build fixes

--- gulp_tasks/client_copy.ts
+++ gulp_tasks/client_copy.ts
@@ -1,10 +1,10 @@
 import merge = require('merge-stream');
 import {join} from 'path';
-import {ENV, CLIENT_SRC, CLIENT_DEST, CLIENT_FONTS_DEST,  NPM_FONTS, TMP_DIR} from './config';
+import {ENV, CLIENT_SRC, CLIENT_CSS_SRC, CLIENT_DEST, CLIENT_CSS_DEST, CLIENT_FONTS_DEST,  NPM_FONTS, TMP_DIR} from './config';
 
 export = function client_copy(gulp, plugins) {
     return function () {
-        return merge(copyHtml(), copyCss(), copyNpmFonts());
+        return merge(copyHtml(), copyCss(), copyAppCss(), copyNpmFonts());
 
         function copyHtml() {
             return gulp
@@ -16,6 +16,12 @@ export = function client_copy(gulp, plugins) {
             return gulp
                 .src(join(CLIENT_SRC, '**/*.css'))
                 .pipe(ENV === 'dev' ? gulp.dest(CLIENT_DEST) : gulp.dest(TMP_DIR));
+        }
+
+        function copyAppCss() {
+            return gulp
+                .src(join(CLIENT_CSS_SRC, '**/*.css'))
+                .pipe(gulp.dest(CLIENT_CSS_DEST));
         }
 
         function copyNpmFonts() {
