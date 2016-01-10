8c26cb27321ae1de838c42e712989314f7286031
Henry Zektser <japhar81@gmail.com>
2016-01-10 12:59:58 -0500

    Fixed gitignore to include mongodb typings

--- server/typings/tsd.d.ts
+++ server/typings/tsd.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="node/node.d.ts" />
 /// <reference path="cookies/cookies.d.ts" />
 /// <reference path="mongodb/mongodb.d.ts" />
+/// <reference path="node/node.d.ts" />
 /// <reference path="validator/validator.d.ts" />
